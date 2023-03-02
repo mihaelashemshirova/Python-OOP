962330758fa882c1d1018f59f1551261fab654f5
Mihaela Shemshirova <121877004+mihaelashemshirova@users.noreply.github.com>
2023-03-02 12:51:20 +0200

    Create Cup.py

---
+++ First Steps in OOP - Exercise/Cup.py
@@ -0,0 +1,18 @@
+class Cup:
+    def __init__(self, size: int, quantity: int):
+        self.size = size
+        self.quantity = quantity
+
+    def status(self):
+        free_space = self.size - self.quantity
+        return abs(free_space)
+
+    def fill(self, quantity: int):
+        if self.size - self.quantity >= quantity:
+            self.quantity += quantity
+
+cup = Cup(100, 50)
+print(cup.status())
+cup.fill(40)
+cup.fill(20)
+print(cup.status())
