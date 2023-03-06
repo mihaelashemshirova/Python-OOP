8a27a0f8e4500f2703c48fc0eff5ad0fd22c4349
Mihaela Shemshirova <121877004+mihaelashemshirova@users.noreply.github.com>
2023-03-06 18:19:58 +0200

    Update Smartphone.py

--- Classes and Objects - Lab/Smartphone.py
+++ Classes and Objects - Lab/Smartphone.py
@@ -1,2 +1,37 @@
 class Smartphone:
+    def __init__(self, memory: int):
+        self.memory = memory
+        self.apps = []
+        self.is_on = False
+
+    def power(self):
+        if not self.is_on:
+            self.is_on = True
+        else:
+            self.is_on = False
+
+    def install(self, app: str, app_memory: int):
+        if self.is_on:
+            if (self.memory - app_memory) > 0:
+                self.apps.append(app)
+                self.memory -= app_memory
+                return f"Installing {app}"
+            else:
+                return f"Not enough memory to install {app}"
+        else:
+            return f"Turn on your phone to install {app}"
+
+    def status(self):
+        apps_count = len(self.apps)
+        memory_left = self.memory
+        return f"Total apps: {apps_count}. Memory left: {memory_left}"
+
+
+smartphone = Smartphone(100)
+print(smartphone.install("Facebook", 60))
+smartphone.power()
+print(smartphone.install("Facebook", 60))
+print(smartphone.install("Messenger", 20))
+print(smartphone.install("Instagram", 40))
+print(smartphone.status())
   
