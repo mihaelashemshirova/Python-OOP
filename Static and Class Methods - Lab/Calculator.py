d2afa2b0d3559eb8f6181b1bf189ea15e264a284
Mihaela Shemshirova <121877004+mihaelashemshirova@users.noreply.github.com>
2023-03-18 14:08:44 +0200

    Create Calculator.py

---
+++ Static and Class Methods - Lab/Calculator.py
@@ -0,0 +1,21 @@
+from typing import List
+from functools import reduce
+
+
+class Calculator:
+
+    @staticmethod
+    def add(*nums: List[int]) -> int:
+        return sum(nums)
+
+    @staticmethod
+    def multiply(*nums: List[int]):
+        return reduce(lambda a, b: a * b, nums)
+
+    @staticmethod
+    def divide(*nums: List[int]):
+        return reduce(lambda a, b: a / b, nums)
+
+    @staticmethod
+    def subtract(*nums: List[int]):
+        return reduce(lambda a, b: a - b, nums)
